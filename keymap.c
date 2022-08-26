1490124f5107a72bbe32e7728332b4a2317de15d
Johannes Hertenstein <git@j6s.dev>
2022-08-26 17:17:50 +0200

    Update keymap

--- keymap.c
+++ keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
   * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
   * |--------+------+------+------+------+------|      |           |  F7  |------+------+------+------+------+--------|
-  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | rShift |
+  * | LShift |   Z  |   X  |   C  |   V  |   B  | F19  |           |      |   N  |   M  |   ,  |   .  |   /  | rShift |
   * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
   *   |lCtrl | lWIN | lALT |(none)|  l1  |                                       |  l1  | rAlt |   [  |   ]  | rCtrl  |
   *   `----------------------------------'                                       `----------------------------------'
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,     KC_1,      KC_2,     KC_3,      KC_4,       KC_5,     KC_6,
     KC_TAB,     KC_Q,      KC_W,     KC_E,      KC_R,       KC_T,     KC_F5,
     KC_BSPC,    KC_A,      KC_S,     KC_D,      KC_F,       KC_G,
-    KC_LSFT,    KC_Z,      KC_X,     KC_C,      KC_V,       KC_B,     _______,
+    KC_LSFT,    KC_Z,      KC_X,     KC_C,      KC_V,       KC_B,     KC_F19,
     KC_LCTRL,   KC_LWIN,   KC_LALT,  KC_NO,     MO(L1),
 
                                                             KC_HOME,  KC_END,
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_H,      KC_J,     KC_K,      KC_L,       KC_SCLN,  KC_QUOT,
     KC_F7,      KC_N,      KC_M,     KC_COMM,   KC_DOT,     KC_SLSH,  KC_RSFT,
                            MO(L1),   KC_RALT,   KC_LBRC,    KC_RBRC,  KC_RCTRL,
-
+        
     KC_LEFT,    KC_RIGHT,
     KC_UP,
     KC_DOWN,    KC_LCTRL,  KC_ENT
